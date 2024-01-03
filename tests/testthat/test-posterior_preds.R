d0442749d706285d8c4a2da6515d392f20168129
Fonti Kar <f.kar@unsw.edu.au>
2024-01-04 10:09:50 +1100

    another unit test

--- tests/testthat/test-posterior_preds.R
+++ tests/testthat/test-posterior_preds.R
@@ -2,3 +2,8 @@ test_that("functions executes", {
   expect_named(augGauss:::Get_Posterior_Preds_by_group(output, "group1"))
   vdiffr::expect_doppelganger("posterior_preds", Plot_Posterior_Preds_by_group(output, "group1"))
 })
+
+
+test_that("throws errors when group name is wrong", {
+  expect_error(Plot_Posterior_Preds_by_group(output, "group_wrong"))
+})
